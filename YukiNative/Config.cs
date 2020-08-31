6acdac8e5cd175413f159d8690d6b24bd7d8ee8d
Yesterday17 <mmf@mmf.moe>
2020-08-31 20:58:26 +0800

    Parse commandline using CommandLineParser.

---
+++ YukiNative/Config.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace YukiNative {
+  public class Config {
+    [Option('l', "listen", HelpText = "Local address to listen.", Default = "http://localhost:8080/")]
+    public string ListenAddr { get; set; }
+
+    [Option('t', "textreactor", HelpText = "Location of TextreactorCLI.exe.")]
+    public string TextreactorLocation { get; set; }
+
+    [Option('d', "directory", HelpText = "Directories to load dlls.")]
+    public IEnumerable<string> DllDirectories { get; set; }
+  }
+}
--- YukiNative/Program.cs
+++ YukiNative/Program.cs
@@ -1,54 +1,37 @@
-﻿using YukiNative.server;
+﻿using System;
+using CommandLine;
+using YukiNative.server;
 using YukiNative.services;
 using YukiNative.utils;
 
 namespace YukiNative {
-  internal enum WorkMode {
-    Http,
-    Websocket
-  }
-
   internal static class Program {
     private static void Main(string[] args) {
-      // Work mode
-      var mode = WorkMode.Http;
-      if (args.Length >= 1 && args[0].ToLower().Equals("websocket")) {
-        mode = WorkMode.Websocket;
-      }
+      Parser.Default.ParseArguments<Config>(args)
+        .WithParsed(options => {
+          // Load directories
+          foreach (var dir in options.DllDirectories) {
+            Library.SetDllDirectory(dir);
+          }
 
-      // Load directories
-      if (args.Length >= 1) {
-        var i = args[0].ToLower().Equals("websocket")
-                || args[0].ToLower().Equals("http")
-          ? 1
-          : 0;
-        for (; i < args.Length; i++) {
-          Library.SetDllDirectory(args[i]);
-        }
-      }
+          var server = new HttpServer();
+          var listen = server
+            .AddRoute("/library", Library.AddLibraryService)
+            .AddRoute("/jbeijing7", JBeijing7.JBeijing7Service)
+            .AddRoute("/mecab", Mecab.MecabService)
+            .AddRoute("/ping", (_, __, response) => response.WriteText("pong"))
+            .AddRoute("/shutdown", (httpServer, request, response) => {
+              // Close response manually before server stops
+              response.Close();
+              // Stop server
+              httpServer.Stop();
+            })
+            .Listen(options.ListenAddr);
 
-      // HTTP Mode
-      if (mode == WorkMode.Http) {
-        var server = new HttpServer();
-        server
-          .AddRoute("/library", Library.AddLibraryService)
-          .AddRoute("/jbeijing7", JBeijing7.JBeijing7Service)
-          .AddRoute("/mecab", Mecab.MecabService)
-          .AddRoute("./ping", (_, __, response) => response.WriteText("pong"))
-          .AddRoute("/shutdown", (httpServer, request, response) => {
-            // Close response manually before server stops
-            response.Close();
-            // Stop server
-            httpServer.Stop();
-          })
-          .Listen("http://localhost:8080/")
-          .GetAwaiter()
-          .GetResult();
-        server.Close();
-      }
-      else {
-        // TODO: Websocket Mode
-      }
+          Console.WriteLine("YukiNative listening on {0}...", options.ListenAddr);
+          listen.GetAwaiter().GetResult();
+          server.Close();
+        });
     }
   }
 }
