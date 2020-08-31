82771ae117adafafa7e3fdccb080e658fbcd4d01
Yesterday17 <mmf@mmf.moe>
2020-08-31 19:56:42 +0800

    TextreactorCLI adapter.

---
+++ Textreactor/TextOutputObject.cs
@@ -0,0 +1,46 @@
+﻿namespace Textreactor {
+  public class TextOutputObject {
+    private ulong _handle;
+    private ulong _pid;
+    private ulong _addr;
+    private ulong _ctx;
+    private ulong _ctx2;
+    private string _name;
+    private string _code;
+    public string Text;
+
+    public TextOutputObject(
+      ulong handle, ulong pid, ulong addr,
+      ulong ctx, ulong ctx2,
+      string name, string code, string text = ""
+    ) {
+      _handle = handle;
+      _pid = pid;
+      _addr = addr;
+      _ctx = ctx;
+      _ctx2 = ctx2;
+      _name = name;
+      _code = code;
+      Text = text;
+    }
+
+    public static TextOutputObject Parse(string line) {
+      var rb = line.IndexOf(']');
+      var args = line.Substring(1, rb - 1).Split(':');
+      var text = line.Substring(rb + 2);
+      return new TextOutputObject(
+        Hex2Num(args[0]), Hex2Num(args[1]), Hex2Num(args[2]),
+        Hex2Num(args[3]), Hex2Num(args[4]),
+        args[5], args[6], text
+      );
+    }
+
+    public void AppendText(string append) {
+      Text += append + "\n";
+    }
+
+    private static ulong Hex2Num(string hex) {
+      return ulong.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+    }
+  }
+}
---
+++ Textreactor/Textreactor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+
+namespace Textreactor {
+  public delegate void TextreactorOutputDelegate(TextOutputObject output);
+
+  public class Textreactor {
+    private readonly Process _cliProcess;
+    private TextOutputObject _currentOutput;
+    public TextreactorOutputDelegate OnTextreactorOutput;
+
+    private string _buffer = "";
+
+    public Textreactor(string cli) {
+      var cliInfo = new ProcessStartInfo {
+        FileName = cli,
+        UseShellExecute = false,
+        RedirectStandardInput = true,
+        RedirectStandardOutput = true,
+        RedirectStandardError = false,
+        CreateNoWindow = true,
+        StandardOutputEncoding = Encoding.Unicode,
+      };
+
+      _cliProcess = new Process {
+        StartInfo = cliInfo,
+      };
+    }
+
+    public void Start() {
+      _cliProcess.Start();
+      _cliProcess.OutputDataReceived += OnProcessOutput;
+      _cliProcess.BeginOutputReadLine();
+    }
+
+    public void Hook(int pid, string code) {
+      if (!code.Contains("/H") && !code.Contains("/R")) {
+        throw new SyntaxErrorException("invalid code");
+      }
+
+      Execute($"{code} -P{pid}");
+    }
+
+    public void Execute(string cmd) {
+      _cliProcess.StandardInput.WriteLine(cmd);
+    }
+
+    public void Attach(int pid) {
+      Execute($"attach -P{pid}");
+    }
+
+    public void Detach(int pid) {
+      Execute($"detach -P{pid}");
+    }
+
+    public void Stop() {
+      _cliProcess.Kill();
+    }
+
+    private void OnProcessOutput(object sender, DataReceivedEventArgs e) {
+      if (string.IsNullOrEmpty(e.Data)) {
+        return;
+      }
+
+      Console.WriteLine(e.Data);
+      OnData(e.Data);
+    }
+
+    private void OnData(string line) {
+      if (line.StartsWith("Usage")) {
+        return;
+      }
+
+      if (line.StartsWith("[")) {
+        _currentOutput = TextOutputObject.Parse(line);
+      }
+      else {
+        _currentOutput.AppendText(line);
+      }
+
+      OnTextreactorOutput?.Invoke(_currentOutput);
+    }
+  }
+}
