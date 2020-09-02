cd1846762f51577fc514ae7ade6b450742433823
Yesterday17 <mmf@mmf.moe>
2020-09-02 16:03:21 +0800

    Fix version & assembly info.

--- Textractor/Properties/AssemblyInfo.cs
+++ Textractor/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("YukiNative")]
+[assembly: AssemblyTitle("Textractor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("YukiNative")]
+[assembly: AssemblyProduct("Textractor")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("42BE7B3B-8F57-4A06-8BE4-67E15EDE49C7")]
+[assembly: Guid("D75BC5EA-4E12-47EF-9FD2-CAD439BCE0EB")]
 
 // Version information for an assembly consists of the following four values:
 //
